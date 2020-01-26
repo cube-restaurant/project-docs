ddbd5b1e0bde7a576d394ddfdb3d011e9306ce70
Harsh Patel <harshppatel2880@gmail.com>
2020-01-25 19:21:11 -0600

    Added basic docs to the project

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  title: 'The Cube Restaurant',
+  title: 'Cube Restaurant',
   description: 'A Scalable web project created for fun!',
-  dest: 'public'
+  dest: 'public',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about.html' },
+      { text: 'Guide', link: '/get-started.html' },
+      { text: 'GitHub', link: 'https://github.com/cube-restaurant' },
+    ],
+    // algolia: {
+    //   apiKey: '<API_KEY>',
+    //   indexName: '<INDEX_NAME>'
+    // }
+  }
 };
